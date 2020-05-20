3d194f8d77cfa5e255e755095bdab715dec79858
Messer <messer124@gmail.com>
2020-05-20 13:38:17 +0300

    update

--- src/main/java/servlet/filter/AdminFilter.java
+++ src/main/java/servlet/filter/AdminFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static java.util.Objects.nonNull;
+
 @WebFilter("/admin")
 public class AdminFilter implements Filter {
     @Override
@@ -19,7 +21,12 @@ public class AdminFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
-        String role = session.getAttribute("role").toString();
+        String role = "";
+
+        if (nonNull(session.getAttribute("role"))) {
+            role = session.getAttribute("role").toString();
+        } else req.getRequestDispatcher("pages/accessError.jsp").forward(req, resp);
+
 
         if (!role.equals("admin")) {
             req.getRequestDispatcher("pages/accessError.jsp").forward(req, resp);
--- src/main/java/servlet/filter/UserFilter.java
+++ src/main/java/servlet/filter/UserFilter.java
@@ -7,6 +7,8 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+import static java.util.Objects.nonNull;
+
 @WebFilter("/user")
 public class UserFilter implements Filter {
     @Override
@@ -19,7 +21,11 @@ public class UserFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
         HttpSession session = req.getSession();
-        String role = session.getAttribute("role").toString();
+        String role = "";
+
+        if (nonNull(session.getAttribute("role"))) {
+            role = session.getAttribute("role").toString();
+        } else req.getRequestDispatcher("pages/accessError.jsp").forward(req, resp);
 
         if (!role.matches("user|admin")) {
             req.getRequestDispatcher("pages/accessError.jsp").forward(req, resp);
